24511e5c2aa62d7ce0f70ff41b44bccd3d374ed1
VICTORY <iarotskaia.vv@mail.ru>
2024-08-06 15:52:45 +1000

    modified

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -1,10 +1,13 @@
-﻿// программа которая принимает на входе два целых числа и проверяте является ли второе число квадратом первого.
+﻿// программа которая принимает на входе два целых числа и проверяте является ли первое число квадратом второгосв.
 // a = 25; b = 5 - yes
 // a = 2; b = 10 - no
 // a = 9; b = -3 - yes
 // a = -3; b = 9 - no
 
+Console.Write("Write first number: ");
 int firstValue = int.Parse(Console.ReadLine());
+
+Console.Write("Write second namber: ");
 int secondValue = Convert.ToInt32(Console.ReadLine());
 
 if(firstValue == secondValue * secondValue)
