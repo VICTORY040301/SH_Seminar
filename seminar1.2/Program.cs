65297a411c6478cd30761b265e82a67c13d96283
VICTORY <iarotskaia.vv@mail.ru>
2024-08-06 16:26:22 +1000

    add seminar1.2

---
+++ seminar1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на входе принимает одно целое число N, 
+// а на выходе показывает все целые значения между N и -N.
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = -number;
+while(i <= number)
+{
+    System.Console.Write(i + " ");
+    i++;
+}
+
+// for(int i = -number; i <= number; i++)
+//    System.Console.Write(i + " ");
